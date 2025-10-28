7719c27abee94f7d5c1ec55ec9c447c1aa6a5b5a
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-10-28 00:44:02 -0700

    create filter query for listings

--- GPUSpecServer/GPUSpecServer/Controllers/ListingsController.cs
+++ GPUSpecServer/GPUSpecServer/Controllers/ListingsController.cs
@@ -22,43 +22,89 @@ namespace GPUSpecServer.Controllers
             _context = context;
         }
 
+        private IQueryable<Listing> FilterQuery(IQueryable<Listing> query, ListingSearchParams searchParams) {
+            if (!string.IsNullOrEmpty(searchParams.manufacturer)) {
+                query = query.Where(l => l.manufacturer.ToLower() == searchParams.manufacturer.ToLower());
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.architecture))
+            {
+                query = query.Where(l => l.architecture.ToLower() == searchParams.architecture.ToLower());
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.generation))
+            {
+                query = query.Where(l => l.generation.ToLower() == searchParams.generation.ToLower());
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.product))
+            {
+                query = query.Where(l => l.Product.Name.ToLower() == searchParams.product.ToLower());
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.chip))
+            {
+                query = query.Where(l => l.Chip.Name.ToLower() == searchParams.chip.ToLower());
+            }
+
+            if (searchParams.min_release_year.HasValue)
+            {
+                query = query.Where(l => l.release_date.HasValue && l.release_date.Value.Year >= searchParams.min_release_year.Value);
+            }
+
+            if (searchParams.max_release_year.HasValue)
+            {
+                query = query.Where(l => l.release_date.HasValue && l.release_date.Value.Year <= searchParams.max_release_year.Value);
+            }
+
+            if (searchParams.min_process_size.HasValue)
+            {
+                query = query.Where(l => l.Chip.process_size.HasValue && l.Chip.process_size >= searchParams.min_process_size.Value);
+            }
+
+            if (searchParams.max_process_size.HasValue)
+            {
+                query = query.Where(l => l.Chip.process_size.HasValue && l.Chip.process_size <= searchParams.max_process_size.Value);
+            }
+
+            if (searchParams.min_memory_size.HasValue)
+            {
+                query = query.Where(l => l.memory_size >= searchParams.min_memory_size.Value);
+            }
+
+            if (searchParams.max_memory_size.HasValue)
+            {
+                query = query.Where(l => l.memory_size <= searchParams.max_memory_size.Value);
+            }
+
+            if (searchParams.min_memory_bus.HasValue)
+            {
+                query = query.Where(l => l.memory_bus.HasValue && l.memory_bus >= searchParams.min_memory_bus.Value);
+            }
+
+            if (searchParams.max_memory_bus.HasValue)
+            {
+                query = query.Where(l => l.memory_bus.HasValue && l.memory_bus <= searchParams.max_memory_bus.Value);
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.memory_type))
+            {
+                query = query.Where(l => l.memory_type.ToLower() == searchParams.memory_type.ToLower());
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.slot_width))
+            {
+                query = query.Where(l => !string.IsNullOrEmpty(l.board_slot_width) && l.board_slot_width.ToLower() == searchParams.slot_width.ToLower());
+            }
+
+            return query;
+        }
+
         // GET: api/Listings
         [HttpGet]
-        public async Task<ActionResult<PagedResult<ListingDTO>>> GetListings([FromQuery] int pageIndex = 1, [FromQuery] int pageSize = 10, [FromQuery] string orderBy = "desc")
+        public async Task<ActionResult<PagedResult<ListingDTO>>> GetListings([FromQuery] ListingSearchParams searchParams, int pageIndex = 1, int pageSize = 10, string orderBy = "desc")
         {
-            var query = _context.Listings.AsNoTracking().Select(ListingDTO.Projection);
-            return await PagedResult<ListingDTO>.CreateAsync(query, pageIndex, pageSize, "release_date", orderBy);
-        }
-
-        // GET: api/Listings/Manufacturer/NVIDIA
-        [HttpGet("Manufacturer/{manufacturer}")]
-        public async Task<ActionResult<PagedResult<ListingDTO>>> GetManufacturerListings(string manufacturer, [FromQuery] int pageIndex = 1, [FromQuery] int pageSize = 10, [FromQuery] string orderBy = "desc")
-        {
-            var query = _context.Listings.AsNoTracking().Where(l => l.manufacturer.ToLower() == manufacturer.ToLower()).Select(ListingDTO.Projection);
-            return await PagedResult<ListingDTO>.CreateAsync(query, pageIndex, pageSize, "release_date", orderBy);
-        }
-
-        // GET: api/Listings/Architecture/NVIDIA
-        [HttpGet("Architecture/{architecture}")]
-        public async Task<ActionResult<PagedResult<ListingDTO>>> GetArchitectureListings(string architecture, [FromQuery] int pageIndex = 1, [FromQuery] int pageSize = 10, [FromQuery] string orderBy = "desc")
-        {
-            var query = _context.Listings.AsNoTracking().Where(l => l.architecture.ToLower() == architecture.ToLower()).Select(ListingDTO.Projection);
-            return await PagedResult<ListingDTO>.CreateAsync(query, pageIndex, pageSize, "release_date", orderBy);
-        }
-
-        // GET: api/Listings/Generation/NVIDIA
-        [HttpGet("Generation/{generation}")]
-        public async Task<ActionResult<PagedResult<ListingDTO>>> GetGenerationListings(string generation, [FromQuery] int pageIndex = 1, [FromQuery] int pageSize = 10, [FromQuery] string orderBy = "desc")
-        {
-            var query = _context.Listings.AsNoTracking().Where(l => l.generation.ToLower() == generation.ToLower()).Select(ListingDTO.Projection);
-            return await PagedResult<ListingDTO>.CreateAsync(query, pageIndex, pageSize, "release_date", orderBy);
-        }
-
-        // GET: api/Listings/Chip/NVIDIA
-        [HttpGet("Chip/{chip}")]
-        public async Task<ActionResult<PagedResult<ListingDTO>>> GetChipListings(string chip, [FromQuery] int pageIndex = 1, [FromQuery] int pageSize = 10, [FromQuery] string orderBy = "desc")
-        {
-            var query = _context.Listings.AsNoTracking().Where(l => l.Chip.Name.ToLower() == chip.ToLower()).Select(ListingDTO.Projection);
+            var query = FilterQuery(_context.Listings.AsNoTracking(), searchParams).Select(ListingDTO.Projection);
             return await PagedResult<ListingDTO>.CreateAsync(query, pageIndex, pageSize, "release_date", orderBy);
         }
 
---
+++ GPUSpecServer/GPUSpecServer/Data/ListingSearchParams.cs
@@ -0,0 +1,25 @@
+﻿namespace GPUSpecServer.Data
+{
+    public class ListingSearchParams
+    {
+        public string? manufacturer {  get; set; }
+        public string? architecture { get; set; }
+        public string? generation { get; set; }
+        public string? product { get; set; }
+        public string? chip { get; set; }
+
+        public int? min_release_year { get; set; }
+        public int? max_release_year { get; set; }
+        public int? min_process_size { get; set; }
+        public int? max_process_size { get; set; }
+
+        public float? min_memory_size { get; set; }
+        public float? max_memory_size { get; set; }
+
+        public float? min_memory_bus { get; set; }
+        public float? max_memory_bus { get; set; }
+
+        public string? memory_type { get; set; }
+        public string? slot_width { get; set; }
+    }
+}
