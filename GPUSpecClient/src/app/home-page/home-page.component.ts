1b9613560bee544ac4aebfe61789bc5c95c9a3ce
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-11-25 11:34:35 -0800

    home page

--- GPUSpecClient/src/app/home-page/home-page.component.ts
+++ GPUSpecClient/src/app/home-page/home-page.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterLink,
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
+  search_name = new FormControl('');
 
+  submit() {}
 }
