37f273eafe8ba4247c159d8fd40a675a2277e56a
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-11-24 18:56:56 -0800

    login html

--- GPUSpecClient/src/app/browse-page/browse-page.component.ts
+++ GPUSpecClient/src/app/browse-page/browse-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { FilterbarComponent } from '../filterbar/filterbar.component';
+import { ListingsComponent } from '../listings/listings.component';
 
 @Component({
   selector: 'app-browse-page',
   standalone: true,
-  imports: [],
+  imports: [FilterbarComponent, ListingsComponent],
   templateUrl: './browse-page.component.html',
-  styleUrl: './browse-page.component.scss'
+  styleUrl: './browse-page.component.scss',
 })
-export class BrowsePageComponent {
-
-}
+export class BrowsePageComponent {}
--- GPUSpecClient/src/app/login-page/login-page.component.ts
+++ GPUSpecClient/src/app/login-page/login-page.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
+  submit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const username = this.loginForm.get('username')?.value;
+    const password = this.loginForm.get('password')?.value;
+  }
 }
