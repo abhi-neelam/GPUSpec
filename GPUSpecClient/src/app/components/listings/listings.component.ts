e129d47af02d1a1804fd4f4750bfe2bab006b3c9
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-11-27 13:29:40 -0800

    fix some ui issues and add sort by date option to browse page

--- GPUSpecClient/src/app/components/listings/listings.component.ts
+++ GPUSpecClient/src/app/components/listings/listings.component.ts
@@ -1,14 +1,37 @@
 import { Component, Input } from '@angular/core';
 import { Listing } from '../../interfaces/listing';
 import { PagedResult } from '../../interfaces/pagedresult';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { SelectOption } from '../../interfaces/selectoption';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-listings',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatSelectModule],
   templateUrl: './listings.component.html',
   styleUrl: './listings.component.scss',
 })
 export class ListingsComponent {
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
+
   @Input({ required: true }) data: PagedResult<Listing> | null = null;
+  selectedSortOption: string = 'desc';
+
+  sortOptions: SelectOption[] = [
+    { value: 'desc', display: 'Newest First' },
+    { value: 'asc', display: 'Oldest First' },
+  ];
+
+  onSubmit() {
+    this.router.navigate(['/browse'], {
+      relativeTo: this.activatedRoute,
+      queryParams: {
+        orderBy: this.selectedSortOption,
+      },
+      queryParamsHandling: 'merge',
+    });
+  }
 }
