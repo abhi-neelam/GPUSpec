d814497ee064033a044e54eab7ee788487e8c4a4
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-11-27 00:01:57 -0800

    pass data input to listings component

--- GPUSpecClient/src/app/components/listings/listings.component.ts
+++ GPUSpecClient/src/app/components/listings/listings.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Listing } from '../../interfaces/listing';
+import { PagedResult } from '../../interfaces/pagedresult';
 
 @Component({
   selector: 'app-listings',
   standalone: true,
   imports: [],
   templateUrl: './listings.component.html',
-  styleUrl: './listings.component.scss'
+  styleUrl: './listings.component.scss',
 })
 export class ListingsComponent {
-
+  @Input({ required: true }) data: PagedResult<Listing> | null = null;
 }
