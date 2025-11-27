afce5c763354aec4134bc510976cf44308d3eaa2
Abhinav Neelam <abhinavneelam949@gmail.com>
2025-11-26 19:27:08 -0800

    create listing options for filtering

--- GPUSpecClient/src/app/components/filterbar/filterbar.component.ts
+++ GPUSpecClient/src/app/components/filterbar/filterbar.component.ts
@@ -4,25 +4,86 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { SelectOption } from '../../interfaces/selectoption';
-import { rawArchitectures, rawManufacturers } from '../../constants/rawoptions';
+import {
+  rawArchitectures,
+  rawManufacturers,
+  rawMemoryTypes,
+} from '../../constants/rawoptions';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  defaultMinMemorySize,
+  defaultMaxMemorySize,
+  defaultMinProcessSize,
+  defaultMaxProcessSize,
+  defaultMinReleaseYear,
+  defaultMaxReleaseYear,
+  defaultArchitecture,
+  defaultManufacturer,
+  defaultMemoryType,
+} from '../../constants/ranges';
 
 @Component({
   selector: 'app-filterbar',
   standalone: true,
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    MatSliderModule,
+    MatButtonModule,
+  ],
   templateUrl: './filterbar.component.html',
   styleUrl: './filterbar.component.scss',
 })
 export class FilterbarComponent {
-  selectedManufacturer: string = 'All Manufacturers';
+  selectedManufacturer: string = '';
+  selectedArchitecture: string = '';
+  selectedMemoryType: string = '';
+
+  minMemorySize: number = defaultMinMemorySize;
+  maxMemorySize: number = defaultMaxMemorySize;
+
+  minProcessSize: number = defaultMinProcessSize;
+  maxProcessSize: number = defaultMaxProcessSize;
+
+  minReleaseYear: number = defaultMinReleaseYear;
+  maxReleaseYear: number = defaultMaxReleaseYear;
 
   manufacturers: SelectOption[] = rawManufacturers.map((item) => ({
-    value: item === 'All Manufacturers' ? '' : item,
+    value: item === defaultManufacturer ? '' : item,
     display: item,
   }));
 
   architectures: SelectOption[] = rawArchitectures.map((item) => ({
-    value: item === 'All Architectures' ? '' : item,
+    value: item === defaultArchitecture ? '' : item,
     display: item,
   }));
+
+  memoryTypes: SelectOption[] = rawMemoryTypes.map((item) => ({
+    value: item === defaultMemoryType ? '' : item,
+    display: item,
+  }));
+
+  get isMemorySizeChanged(): boolean {
+    return (
+      this.minMemorySize !== defaultMinMemorySize ||
+      this.maxMemorySize !== defaultMaxMemorySize
+    );
+  }
+
+  get isProcessSizeChanged(): boolean {
+    return (
+      this.minProcessSize !== defaultMinProcessSize ||
+      this.maxProcessSize !== defaultMaxProcessSize
+    );
+  }
+
+  get isReleaseYearChanged(): boolean {
+    return (
+      this.minReleaseYear !== defaultMinReleaseYear ||
+      this.maxReleaseYear !== defaultMaxReleaseYear
+    );
+  }
 }
---
+++ GPUSpecClient/src/app/constants/ranges.ts
@@ -0,0 +1,12 @@
+export let defaultMinMemorySize: number = 0;
+export let defaultMaxMemorySize: number = 48;
+
+export let defaultMinProcessSize: number = 0;
+export let defaultMaxProcessSize: number = 800;
+
+export let defaultMinReleaseYear: number = 1986;
+export let defaultMaxReleaseYear: number = 2025;
+
+export let defaultManufacturer: string = 'All Manufacturers';
+export let defaultArchitecture: string = 'All Architectures';
+export let defaultMemoryType: string = 'All Memory Types';
